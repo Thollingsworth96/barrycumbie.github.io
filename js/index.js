fa5102085fd74f2445369c53e482d87f309666e0
Thollingsworth96 <37086910+Thollingsworth96@users.noreply.github.com>
2018-03-07 15:54:22 -0600

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var allDOMElements = document;
+console.log(allDOMElements);
+var all = document.getElementsByTagName("*");
+
+for (var i=0, max=all.length; i < max; i++) {
+     // Do something with the element here
+	console.log('Am i in the loop?');
+}
