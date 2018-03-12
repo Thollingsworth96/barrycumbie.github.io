32d576e6e599c1565a627bc770542b80b4c28a98
Thollingsworth96 <37086910+Thollingsworth96@users.noreply.github.com>
2018-03-12 15:50:03 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-var allDOMElements = document;
-console.log(allDOMElements);
+
+$(document).ready(function() {
+//var allDOMElements = document;
+//console.log(allDOMElements);
 var all = document.getElementsByTagName("*");
 
 for (var i=0, max=all.length; i < max; i++) {
      // Do something with the element here
-	console.log('Am i in the loop?');
+	console.log(all[i].tagName);
+	
 }
+	
+});	
